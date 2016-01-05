3b83fe4c33379d6b6e2546e025cd55ec8799fc7a
sharat910 <sharat910@gmail.com>
2016-01-05 20:47:27 +0530

    After the video call

--- portal/validators.py
+++ portal/validators.py
@@ -4,4 +4,4 @@ def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.pdf', '.doc', '.docx']
     if not ext in valid_extensions:
-        raise ValidationError(u'Unsupported file extension.')
+        raise ValidationError(u'Unsupported file extension.Please upload a text document.')
